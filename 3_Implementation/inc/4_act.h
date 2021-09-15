a2379e424da3023baee80b693c4ec65bdc2c3232
bhargavi-muppirisetti <89730441+bhargavi-muppirisetti@users.noreply.github.com>
2021-09-15 21:34:41 +0530

    Update 4_act.h

--- 3_Implementation/inc/4_act.h
+++ 3_Implementation/inc/4_act.h
@@ -24,14 +24,14 @@
 /**
  * @brief Initialize usart
  * 
- * @param ubrr_value 
+ *
  */
 void USARTInit(uint16_t ubrr_value);
 
 /**
  * @brief write to usart
  * 
- * @param data 
+ * 
  */
 
 void USARTWriteChar(int data);
